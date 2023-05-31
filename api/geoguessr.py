86251d92de763e1523596382237263ed9f9f5169
Evan Aparri <evanaparri@gmail.com>
2023-05-31 14:49:09 -0700

    code now works

--- api/geoguessr.py
+++ api/geoguessr.py
@@ -16,34 +16,35 @@ class GeoguessrAPI:
 
             username = body.get('username')
             if username is None or len(username) <= 0:
-                return {'message': 'Username missing'}, 400
+                return {'message': 'username missing'}, 210
 
+            # Change later to exclude negative scores
             score = body.get('score')
             if score is None or int(score) <= 0:
-                return {'message': 'Score does not exist, is missing, or is invalid'}, 400
+                return {'message': 'Score does not exist, is missing, or is invalid'}, 210
 
             game_datetime = body.get('game_datetime')
 
             uo = Geoguessr(
                 username=username,
-                score=score
-            )
+                score=score)
 
+            
             if game_datetime is not None:
                 try:
                     uo.dos = datetime.strptime(game_datetime, '%m-%d-%Y').date()
                 except ValueError:
-                    return {'message': 'Invalid date format. Must be mm-dd-yyyy'}, 400
+                    return {'message': 'Invalid date format. Must be mm-dd-yyyy'}, 210
 
             user = uo.create()
             if user:
-                return jsonify(user.make_dict()), 201
-            return {'message': 'Failed to create user'}, 500
+                return jsonify(user.make_dict())
+            return {'message': 'Failed to create user'}, 210
 
         def get(self):
             geoguessrs = Geoguessr.query.all()
             json_ready = [user.make_dict() for user in geoguessrs]
-            return jsonify(json_ready), 200
+            return jsonify(json_ready)
         
         def put(self):
             body = request.get_json()
@@ -51,7 +52,7 @@ class GeoguessrAPI:
             score = body.get('score')
             game_datetime = body.get('game_datetime')
             data = body.get('data')
-            player = Geoguessr.query.get(username) # get the player (using the uid in this case)
+            player = Geoguessr.query.get(username)  # get the player (using the uid in this case)
             player.update(data)
             user = Geoguessr.query.filter_by(username=username).first()
             if not user:
@@ -67,13 +68,18 @@ class GeoguessrAPI:
                     user.game_datetime = datetime.strptime(game_datetime, '%m-%d-%Y').date()
                 except ValueError:
                     return {'message': 'Invalid date format. Must be mm-dd-yyyy'}, 400
+
             return f"{player.read()} Updated"
 
         def delete(self):
             body = request.get_json()
-            uid = body.get('username')
-            player = Geoguessr.query.get(uid)
-            player.delete()
-            return f"{player.read()} Has been deleted"
+            username = body.get('username')
+            user = Geoguessr.query.filter_by(username=username).first()
+            if not user:
+                return {'message': 'User not found'}, 404
+
+            user.delete()
+            return f"{user.read()} has been deleted"
+
 
     api.add_resource(GeoguessrCR, '/')
